1aa3603fe3704ab0ad323992d325cb437795cf2e
alperen3160 <ilteralperen56@gmail.com>
2024-11-11 19:32:47 +0300

    Create healtar have bug.lua

---
+++ healtar have bug.lua
@@ -0,0 +1,15 @@
+local Esp = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/x114/RobloxScripts/main/OpenSourceEsp"))()
+-- Boxes --
+Esp.Box = false
+Esp.BoxColor = Color3.fromRGB(255,255,255)
+Esp.BoxOutline = true
+Esp.BoxOutlineColor = Color3.fromRGB(0,0,0)
+-- HealthBars --
+Esp.HealthBar = true
+Esp.HealthBarSide = "left" -- Left,Bottom,Right
+-- Names --
+Esp.Names = false
+Esp.NamesColor = Color3.fromRGB(255,255,255)
+Esp.NamesOutline = true
+Esp.NamesFont = 2
+Esp.NamesSize = 13
