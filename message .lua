f95651c4ca69f158989a9002a684a31396fdb9bc
alperen3160 <ilteralperen56@gmail.com>
2024-11-12 11:53:02 +0300

    Create message .lua

---
+++ message .lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+Notification:Notify(
+    {Title = "proxiesHack msg", Description = "Hack loaded! By alperen3160"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
