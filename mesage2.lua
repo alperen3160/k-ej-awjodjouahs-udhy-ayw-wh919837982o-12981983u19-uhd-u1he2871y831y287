e3d3e2a2e9355ade60ff7d8424b981faaec35eb8
alperen3160 <ilteralperen56@gmail.com>
2024-11-14 13:29:24 +0300

    Create mesage2.lua

---
+++ mesage2.lua
@@ -0,0 +1,8 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+Notification:Notify(
+    {Title = "proxiesHack msg", Description = "only use 1 seccond if oyu 2-3 second use ur kick."},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
+)
